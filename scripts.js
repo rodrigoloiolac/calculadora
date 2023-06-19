bc98c961fac3ac99e01efff07e61598175c99a85
rodrigoloiolac <137116798+rodrigoloiolac@users.noreply.github.com>
2023-06-19 18:14:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,72 @@
+let um = document.querySelector("#um");
+let dois = document.querySelector("#dois");
+let três = document.querySelector("#três");
+let quatro = document.querySelector("#quatro");
+let cinco = document.querySelector("#cinco");
+let seis = document.querySelector("#seis");
+let sete = document.querySelector("#sete");
+let oito = document.querySelector("#oito");
+let nove = document.querySelector("#nove");
+let dez = document.querySelector("#dez");
+let zero = document.querySelector("#zero");
+let decimal = document.querySelector("#decimal");
+
+let somar = document.querySelector("#somar");
+let subtrair = document.querySelector("#subtrair");
+let multiplicar = document.querySelector("#multiplicar");
+let dividir = document.querySelector("#dividir");
+let limpar = document.querySelector("#limpar");
+let igual = document.querySelector("#igual");
+let resultado = document.querySelector("#resultado");
+
+zero.addEventListener('click', function() {
+     resultado.value += "0";
+})
+um.addEventListener('click', function() {
+    resultado.value += "1";
+})
+dois.addEventListener('click', function() {
+    resultado.value += "2";
+})
+três.addEventListener('click', function() {
+    resultado.value += "3";
+})
+quatro.addEventListener('click', function() {
+    resultado.value += "4";
+})
+cinco.addEventListener('click', function() {
+    resultado.value += "5";
+})
+seis.addEventListener('click', function() {
+    resultado.value += "6";
+})
+sete.addEventListener('click', function() {
+    resultado.value += "7";
+})
+oito.addEventListener('click', function() {
+    resultado.value += "8";
+})
+nove.addEventListener('click', function() {
+    resultado.value += "9";
+})
+somar.addEventListener('click', function() {
+    resultado.value += "+";
+})
+subtrair.addEventListener('click', function() {
+    resultado.value += "-";
+})
+multiplicar.addEventListener('click', function() {
+    resultado.value += "*";
+})
+dividir.addEventListener('click', function() {
+    resultado.value += "/";
+})
+decimal.addEventListener('click', function() {
+    resultado.value += ",";
+})
+igual.addEventListener('click', function() {
+    resultado.value = eval(resultado.value);
+})
+limpar.addEventListener('click', function() {
+    resultado.value = "";
+})
